51428c6790f48c57a1fa2bc304362bc4f24daad7
Sanjita Thapa Magar <stmofficial@gmail.com>
2021-05-10 07:57:23 +0545

    Updated name.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
+import React, {Component} from 'react';
 import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
-import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
@@ -45,9 +41,6 @@ class App extends Component {
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
