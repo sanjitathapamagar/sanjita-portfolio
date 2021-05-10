2ec90ea3859d96eedaab6e0018ef8ad00a22965b
Sanjita Thapa Magar <stmofficial@gmail.com>
2021-05-10 08:29:07 +0545

    Updated name.

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,7 +21,7 @@ class About extends Component {
             <section id="about">
                 <div className="row">
                     <div className="three columns">
-                        <img className="profile-pic" src={profilepic} alt="Tim Baker Profile Pic"/>
+                        <img className="profile-pic" src={profilepic} alt="Sanjita Pic"/>
                     </div>
                     <div className="nine columns main-col">
                         <h2>About Me</h2>
